11a4707726c2274c2640ba30d9f897fe7bdfb3ee
Denis Espanhol <denisgfespanhol@gmail.com>
2024-07-30 14:25:47 -0300

    Add Dg tween library and animate bttns scale

--- LadyCactusAgainstTheBeauty/Assets/Scripts/UI/ButtonsHover.cs
+++ LadyCactusAgainstTheBeauty/Assets/Scripts/UI/ButtonsHover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
+using DG.Tweening;
 
 public class ButtonsHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -11,23 +12,28 @@ public class ButtonsHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     [SerializeField] private TextMeshProUGUI _textBttn;
 
     private Color _currentColor;
+    private Vector3 _currentScale;
     private Image _bttn;
 
     private void Awake()
     {
         _bttn = GetComponent<Image>();
         _currentColor = _bttn.color;
+        _currentScale = transform.localScale;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         _bttn.color = _hoverColor;
         _textBttn.color = _currentColor;
+        transform.DOScale(1.2f, 0.1f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        DOTween.KillAll();
         _bttn.color = _currentColor;
         _textBttn.color = _hoverColor;
+        transform.DOScale(_currentScale, 0.1f);
     }
 }
